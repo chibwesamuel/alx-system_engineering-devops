e622a5feb4cfa0ffa793bd0c6198d7eb0feec4b0
chibwesamuel <smchibwe@gmail.com>
2023-03-24 08:55:32 -0700

    Task 0 fix

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,8 +1,8 @@
 file { 'school':
 	ensure => 'present',
-	path => '/tmp/school',
-	mode => '0744',
-	group => 'www-data',
-	owner => 'www-data',
 	content => 'I love Puppet',
+	group => 'www-data',
+	mode => '0744',
+	owner => 'www-data',
+	path => '/tmp/school',
 }
