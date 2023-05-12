7bdb7aca6dc8c90ab2efdb8c59635b6027d1f52b
chibwesamuel <smchibwe@gmail.com>
2023-05-12 01:11:53 -0700

    Took care of requirements

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,15 +1,14 @@
 # Fixes Apache 500 error.
-
 file { '/etc/apache2/sites-available/000-default.conf':
 	ensure  => present,
 	content => '
-		<VirtualHost *:80>
-			ServerAdmin webmaster@localhost
-			DocumentRoot /var/www/html
-			ErrorLog ${APACHE_LOG_DIR}/error.log
-			CustomLog ${APACHE_LOG_DIR}/access.log combined
-		</VirtualHost>
-',
+	<VirtualHost *:80>
+		ServerAdmin webmaster@localhost
+		DocumentRoot /var/www/html
+		ErrorLog ${APACHE_LOG_DIR}/error.log
+		CustomLog ${APACHE_LOG_DIR}/access.log combined
+	</VirtualHost>
+	',
 	owner   => 'root',
 	group   => 'root',
 	mode    => '0644',
