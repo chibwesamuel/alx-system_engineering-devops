f42da0c86ec1af03354168c4ae0180129d8ceb35
chibwesamuel <smchibwe@gmail.com>
2023-05-15 00:03:36 -0700

    Fixed spacing issues

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,20 +1,21 @@
 # Fixes an Apache 500 error
 file { '/etc/apache2/sites-available/000-default.conf':
-	ensure  => present,
-	content => '
-	<VirtualHost *:80>
-		ServerAdmin webmaster@localhost
-		DocumentRoot /var/www/html
-		ErrorLog ${APACHE_LOG_DIR}/error.log
-		CustomLog ${APACHE_LOG_DIR}/access.log combined
-	</VirtualHost>
-	',
-	owner   => 'root',
-	group   => 'root',
-	mode    => '0644',
+  ensure  => present,
+  content => '
+    <VirtualHost *:80>
+      ServerAdmin webmaster@localhost
+      DocumentRoot /var/www/html
+      ErrorLog ${APACHE_LOG_DIR}/error.log
+      CustomLog ${APACHE_LOG_DIR}/access.log combined
+    </VirtualHost>
+  ',
+  owner   => 'root',
+  group   => 'root',
+  mode    => '0644',
+  notify  => Service['apache2'],  # Add the notify attribute to trigger service restart
 }
 
 service { 'apache2':
-	ensure  => running,
-	require => File['/etc/apache2/sites-available/000-default.conf'],
+  ensure  => running,
+  enable  => true,  # Ensure the service is enabled on system boot
 }
