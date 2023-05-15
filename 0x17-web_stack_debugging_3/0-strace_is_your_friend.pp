e2738c4020bd4793e09a432c29afca7e4c3a0406
chibwesamuel <smchibwe@gmail.com>
2023-05-15 00:12:38 -0700

    Tab issues fix

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,21 +1,21 @@
 # Fixes an Apache 500 error
 file { '/etc/apache2/sites-available/000-default.conf':
-  ensure  => present,
-  content => '
-    <VirtualHost *:80>
-      ServerAdmin webmaster@localhost
-      DocumentRoot /var/www/html
-      ErrorLog ${APACHE_LOG_DIR}/error.log
-      CustomLog ${APACHE_LOG_DIR}/access.log combined
-    </VirtualHost>
-  ',
-  owner   => 'root',
-  group   => 'root',
-  mode    => '0644',
-  notify  => Service['apache2'],  # Add the notify attribute to trigger service restart
+	ensure  => present,
+	content => '
+	<VirtualHost *:80>
+		ServerAdmin webmaster@localhost
+		DocumentRoot /var/www/html
+		ErrorLog ${APACHE_LOG_DIR}/error.log
+		CustomLog ${APACHE_LOG_DIR}/access.log combined
+	</VirtualHost>
+	',
+	owner   => 'root',
+	group   => 'root',
+	mode    => '0644',
+	notify  => Service['apache2'],  # Add the notify attribute to trigger service restart
 }
 
 service { 'apache2':
-  ensure  => running,
-  enable  => true,  # Ensure the service is enabled on system boot
+	ensure  => running,
+	enable  => true,  # Ensure the service is enabled on system boot
 }
